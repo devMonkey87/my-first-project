afd8dffd233c0c179e4fd8ba2c199b3c8269d512
devMonkey87 <jlandetemunoz@icloud.com>
2024-07-27 14:48:23 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NxBadgeModule } from '@aposin/ng-aquila/badge';
+import { NxDocumentationIconModule } from '@aposin/ng-aquila/documentation-icons';
+import { NxIconModule } from '@aposin/ng-aquila/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestTodoComponent } from './test-todo/test-todo.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TestTodoComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NxDocumentationIconModule,
+    NxIconModule,
+    NxBadgeModule,  
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
